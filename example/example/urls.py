c8af730ebeb28e7ad19b63473f0c6d77514b2a12
Adam Johnson <me@adamj.eu>
2021-02-07 11:45:07 +0000

    Add partial rendering example (#30)
    This takes a slightly different approach to the mixin-based one in #5 that doesn't need any new features in django-htmx.
    
    By using Django's ability to dynamically select a base template in `{% extends %}`, we avoid a need for `{% htmx_include %}`.
    
    By using the inheritance of htmx attributes, we avoid the need for the `htmx_attrs` tag to repeat common attributes.

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from example.core.views import index, middleware_tester, middleware_tester_table
+from example.core.views import (
+    index,
+    middleware_tester,
+    middleware_tester_table,
+    partial_rendering,
+)
 
 urlpatterns = [
     path("", index),
     path("middleware-tester/", middleware_tester),
     path("middleware-tester/table/", middleware_tester_table),
+    path("partial-rendering/", partial_rendering),
 ]
