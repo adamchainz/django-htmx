acb5236b54fa9ea5435b6f5f41be56fe306d4b25
Adam Johnson <me@adamj.eu>
2022-03-01 12:01:50 +0000

    Remove static dir declaration to avoid warning

--- docs/conf.py
+++ docs/conf.py
@@ -53,12 +53,6 @@ exclude_patterns = [
 #
 html_theme = "furo"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-
 intersphinx_mapping = {
     "https://docs.python.org/": None,
     "https://docs.djangoproject.com/en/dev/": (
