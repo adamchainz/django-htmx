3edc9a5e6f8eaf9dbe39029070c2863a11929f04
Adam Johnson <me@adamj.eu>
2023-10-20 17:41:29 +0100

    Formalize documentation parameter lists
    Expanding the parameters breaks out the type hints that otherwise appear as a massive unreadable block.

--- docs/conf.py
+++ docs/conf.py
@@ -53,6 +53,8 @@ exclude_patterns = [
     "_build",
 ]
 
+autodoc_typehints = "description"
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
