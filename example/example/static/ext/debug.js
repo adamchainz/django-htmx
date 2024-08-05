75e2493af5bc83715ebe9112b41e225e48c4aa74
Adam Johnson <me@adamj.eu>
2024-08-05 11:39:17 +0100

    Upgrade example project dependencies (#468)

--- example/example/static/ext/debug.js
+++ example/example/static/ext/debug.js
@@ -1,11 +1,11 @@
 htmx.defineExtension('debug', {
-    onEvent: function (name, evt) {
-        if (console.debug) {
-            console.debug(name, evt);
-        } else if (console) {
-            console.log("DEBUG:", name, evt);
-        } else {
-            throw "NO CONSOLE SUPPORTED"
-        }
+  onEvent: function(name, evt) {
+    if (console.debug) {
+      console.debug(name, evt)
+    } else if (console) {
+      console.log('DEBUG:', name, evt)
+    } else {
+      throw new Error('NO CONSOLE SUPPORTED')
     }
-});
+  }
+})
