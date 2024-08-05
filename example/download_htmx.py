e06fd0250bf1bf10c8b72463241c03c6c3b239fe
Adam Johnson <me@adamj.eu>
2024-08-05 11:32:38 +0100

    Upgrade example project dependencies

--- example/download_htmx.py
+++ example/download_htmx.py
@@ -1,48 +1,51 @@
 #!/usr/bin/env python
 """
-Download the given htmx version and the extensions we're using.
+Download the htmx version and the extensions we're using.
 
 This is only intended for maintaining the example app.
 """
 from __future__ import annotations
 
-import argparse
 import subprocess
 
 
-def main(argv: list[str] | None = None) -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("version", help="e.g. 1.0.1")
-    args = parser.parse_args(argv)
-    version: str = args.version
-
-    download_file(version, "htmx.js")
-    download_file(version, "ext/debug.js")
-    download_file(version, "ext/event-header.js")
+def main() -> int:
+    # Per: https://htmx.org/docs/#installing
+    # Note using @ to pin version because htmx hasn’t made v2 the “latest” at
+    # time of writing.
+    download_file(
+        "https://unpkg.com/htmx.org@2.0.1/dist/htmx.js",
+        "htmx.js",
+    )
+    # Per: https://github.com/bigskysoftware/htmx-extensions/tree/main/src/debug
+    download_file(
+        "https://unpkg.com/htmx-ext-debug/debug.js",
+        "ext/debug.js",
+    )
+    # Per: https://github.com/bigskysoftware/htmx-extensions/tree/main/src/event-header
+    download_file(
+        "https://unpkg.com/htmx-ext-event-header/event-header.js",
+        "ext/event-header.js",
+    )
 
     print("✅")
     return 0
 
 
-def download_file(version: str, name: str) -> None:
+def download_file(url: str, name: str) -> None:
     print(f"{name}...")
     proc = subprocess.run(
         [
             "curl",
             "--fail",
             "--location",
-            f"https://unpkg.com/htmx.org@{version}/dist/{name}",
+            url,
             "-o",
             f"example/static/{name}",
         ],
     )
     if proc.returncode != 0:
         raise SystemExit(1)
-    # Fix lack of trailing newline in minified files as otherwise pre-commit
-    # has to fix it.
-    if name.endswith(".min.js"):
-        with open(f"example/static/{name}", "a") as fp:
-            fp.write("\n")
 
 
 if __name__ == "__main__":
